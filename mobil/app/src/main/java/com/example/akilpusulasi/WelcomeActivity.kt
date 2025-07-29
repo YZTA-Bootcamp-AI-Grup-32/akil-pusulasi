a61b214fb94d657c1bf9e428971553003ab2a9b6
Ismail Temuroglu <ismailtemuroglu2819@gmail.com>
2025-07-29 22:01:58 +0300

    refactor: add delay for chaning into the main activity

--- mobil/app/src/main/java/com/example/akilpusulasi/WelcomeActivity.kt
+++ mobil/app/src/main/java/com/example/akilpusulasi/WelcomeActivity.kt
@@ -1,12 +1,17 @@
 package com.example.akilpusulasi
 
+import android.content.Intent // Import Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.akilpusulasi.R
-
 
 class WelcomeActivity : AppCompatActivity() {
+
+    // Define the duration of the welcome screen in milliseconds
+    private val WELCOME_SCREEN_DURATION: Long = 3000 // 3 seconds
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
@@ -28,5 +33,13 @@ class WelcomeActivity : AppCompatActivity() {
 
         // Mesajı ekranda göster
         welcomeText.text = message
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this@WelcomeActivity, MainActivity::class.java)
+            startActivity(intent)
+
+        }, WELCOME_SCREEN_DURATION)
     }
 }
