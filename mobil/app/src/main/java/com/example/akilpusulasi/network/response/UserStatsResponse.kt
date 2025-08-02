8ac0953aef2a6efec59afa39012e388bc2c543c9
Ismail Temuroglu <ismailtemuroglu2819@gmail.com>
2025-08-02 22:47:24 +0300

    refactor/ update graph

--- mobil/app/src/main/java/com/example/akilpusulasi/network/response/UserStatsResponse.kt
+++ mobil/app/src/main/java/com/example/akilpusulasi/network/response/UserStatsResponse.kt
@@ -2,10 +2,21 @@ package com.example.akilpusulasi.network.response
 
 import com.google.gson.annotations.SerializedName
 
+data class GameScoreData(
+    @SerializedName("score")
+    val score: Int,
+
+    @SerializedName("level")
+    val level: Int
+)
+
 data class UserStatsResponse(
     @SerializedName("journal_streak")
     val journalStreak: Int,
 
     @SerializedName("last_five_scores")
-    val lastFiveScores: List<Int>
+    val lastFiveScores: List<Int>,
+
+    @SerializedName("last_ten_game_stats")
+    val lastTenGameStats: List<GameScoreData> // New field for the graph
 )
